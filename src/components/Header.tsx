59c61fd5fb3d2e6c359130746f76be491b658606
Sumra-Awan <sumrajamil5@gmail.com>
2024-10-05 02:14:28 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,9 @@
 
 import React from 'react';
 
-const Header: React.FC = () => {
-  return (
-    <header>
-      <h1>Welcome to My Next.js App</h1>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
+function Header(){
+return(
+<h1>This is Header</h1>
+);
+}
+export default Header
