0cfda40cab7b96c74dae1d9de334d8ef2106f066
Sumra-Awan <sumrajamil5@gmail.com>
2024-10-05 02:15:43 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 
-const About: React.FC = () => {
-  return (
-    <section>
-      <h2>About This App</h2>
-      <p>This is a Next.js application</p>
-    </section>
-  );
-};
-
-export default About;
+function About(){
+    return(
+        <>
+        <h1>This is About page</h1>
+        </>
+    )
+}
+export default About
